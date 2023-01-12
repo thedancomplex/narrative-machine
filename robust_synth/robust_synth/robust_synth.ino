4b34398cf92d1e565d1e957d9152c200d2df7254
Daniel M. Lofaro <dan@danlofaro.com>
2023-01-11 21:41:00 -0500

    the major plumming seems to be done

--- robust_synth/robust_synth/robust_synth.ino
+++ robust_synth/robust_synth/robust_synth.ino
@@ -5,11 +5,8 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveformDc     VCO1_f;            //xy=107,105
-AudioSynthWaveformDc     VCO3_f; //xy=112,728
-AudioSynthWaveformDc     VCO2_f; //xy=114,421
-AudioSynthNoiseWhite     noise1;         //xy=124,1003
-AudioSynthWaveformModulated VCO1_SINE;   //xy=310,140
+AudioSynthNoiseWhite     NOISE;         //xy=124,1003
+AudioSynthWaveformModulated VCO1_SINE;   //xy=310,138.75
 AudioSynthWaveformModulated VCO1_SAW;   //xy=314,197
 AudioSynthWaveformModulated VCO3_SINE; //xy=315,763
 AudioSynthWaveformModulated VCO2_SINE; //xy=317,456
@@ -26,13 +23,12 @@ AudioMixer4              VCO3_MIX; //xy=580,847
 AudioMixer4              VCO2_MIX; //xy=582,540
 AudioAmplifier           VCO1_AMP;           //xy=745,222.25
 AudioAmplifier           NOISE_AMP;           //xy=744.0000610351562,1011
-AudioAmplifier           VCO3_AMP_1; //xy=750,845.25
+AudioAmplifier           VCO3_AMP; //xy=750,845.25
 AudioAmplifier           VCO2_AMP; //xy=752,538.25
 AudioMixer4              VCO_MIXER;         //xy=1042.25,244.75003051757812
 AudioAmplifier           AMP_PRE_FILTER;           //xy=1266.7500915527344,385.75
 AudioAmplifier           VCO3_AMP_2;           //xy=1278.750244140625,831.25
-AudioSynthWaveformDc     FREQ_FILTER;            //xy=1292.2500610351562,480.25
-AudioFilterStateVariable filter1;        //xy=1494.5,326.25
+AudioFilterStateVariable FILTER;        //xy=1494.5,326.25
 AudioMixer4              MIXER_FILTER;         //xy=1675.5001220703125,333.0000305175781
 AudioAmplifier           AMP_POST_FILTER;           //xy=1902.9999389648438,363.7143249511719
 AudioEffectEnvelope      ENVELOPE;      //xy=2122.5,300
@@ -40,76 +36,71 @@ AudioAmplifier           AMP_ENVELOPE;           //xy=2307.5,301.25
 AudioMixer4              MIXER_FINAL;         //xy=2525,382.5
 AudioAmplifier           AMP_FINAL;           //xy=2703.75,382.5
 AudioOutputI2S           i2s1;           //xy=2867.8570556640625,384.46429443359375
-AudioConnection          patchCord1(VCO1_f, 0, VCO1_SINE, 0);
-AudioConnection          patchCord2(VCO1_f, 0, VCO1_SAW, 0);
-AudioConnection          patchCord3(VCO1_f, 0, VCO1_SQUARE, 0);
-AudioConnection          patchCord4(VCO1_f, 0, VCO1_TRIANGLE, 0);
-AudioConnection          patchCord5(VCO3_f, 0, VCO3_SINE, 0);
-AudioConnection          patchCord6(VCO3_f, 0, VCO3_SAW, 0);
-AudioConnection          patchCord7(VCO3_f, 0, VCO3_SQUARE, 0);
-AudioConnection          patchCord8(VCO3_f, 0, VCO3_TRIANGLE, 0);
-AudioConnection          patchCord9(VCO2_f, 0, VCO2_SINE, 0);
-AudioConnection          patchCord10(VCO2_f, 0, VCO2_SAW, 0);
-AudioConnection          patchCord11(VCO2_f, 0, VCO2_SQUARE, 0);
-AudioConnection          patchCord12(VCO2_f, 0, VCO2_TRIANGLE, 0);
-AudioConnection          patchCord13(noise1, NOISE_AMP);
-AudioConnection          patchCord14(VCO1_SINE, 0, VCO1_MIX, 0);
-AudioConnection          patchCord15(VCO1_SAW, 0, VCO1_MIX, 1);
-AudioConnection          patchCord16(VCO3_SINE, 0, VCO3_MIX, 0);
-AudioConnection          patchCord17(VCO2_SINE, 0, VCO2_MIX, 0);
-AudioConnection          patchCord18(VCO3_SAW, 0, VCO3_MIX, 1);
-AudioConnection          patchCord19(VCO2_SAW, 0, VCO2_MIX, 1);
-AudioConnection          patchCord20(VCO1_SQUARE, 0, VCO1_MIX, 2);
-AudioConnection          patchCord21(VCO1_TRIANGLE, 0, VCO1_MIX, 3);
-AudioConnection          patchCord22(VCO3_SQUARE, 0, VCO3_MIX, 2);
-AudioConnection          patchCord23(VCO2_SQUARE, 0, VCO2_MIX, 2);
-AudioConnection          patchCord24(VCO3_TRIANGLE, 0, VCO3_MIX, 3);
-AudioConnection          patchCord25(VCO2_TRIANGLE, 0, VCO2_MIX, 3);
-AudioConnection          patchCord26(VCO1_MIX, VCO1_AMP);
-AudioConnection          patchCord27(VCO3_MIX, VCO3_AMP_1);
-AudioConnection          patchCord28(VCO2_MIX, VCO2_AMP);
-AudioConnection          patchCord29(VCO1_AMP, 0, VCO_MIXER, 0);
-AudioConnection          patchCord30(NOISE_AMP, 0, VCO_MIXER, 3);
-AudioConnection          patchCord31(VCO3_AMP_1, 0, VCO_MIXER, 2);
-AudioConnection          patchCord32(VCO3_AMP_1, VCO3_AMP_2);
-AudioConnection          patchCord33(VCO2_AMP, 0, VCO_MIXER, 1);
-AudioConnection          patchCord34(VCO_MIXER, AMP_PRE_FILTER);
-AudioConnection          patchCord35(AMP_PRE_FILTER, 0, filter1, 0);
-AudioConnection          patchCord36(AMP_PRE_FILTER, 0, MIXER_FINAL, 2);
-AudioConnection          patchCord37(VCO3_AMP_2, 0, MIXER_FILTER, 3);
-AudioConnection          patchCord38(VCO3_AMP_2, 0, MIXER_FINAL, 3);
-AudioConnection          patchCord39(FREQ_FILTER, 0, filter1, 1);
-AudioConnection          patchCord40(filter1, 0, MIXER_FILTER, 0);
-AudioConnection          patchCord41(filter1, 1, MIXER_FILTER, 1);
-AudioConnection          patchCord42(filter1, 2, MIXER_FILTER, 2);
-AudioConnection          patchCord43(MIXER_FILTER, AMP_POST_FILTER);
-AudioConnection          patchCord44(AMP_POST_FILTER, ENVELOPE);
-AudioConnection          patchCord45(AMP_POST_FILTER, 0, MIXER_FINAL, 1);
-AudioConnection          patchCord46(ENVELOPE, AMP_ENVELOPE);
-AudioConnection          patchCord47(AMP_ENVELOPE, 0, MIXER_FINAL, 0);
-AudioConnection          patchCord48(MIXER_FINAL, AMP_FINAL);
-AudioConnection          patchCord49(AMP_FINAL, 0, i2s1, 0);
-AudioConnection          patchCord50(AMP_FINAL, 0, i2s1, 1);
+AudioConnection          patchCord1(NOISE, NOISE_AMP);
+AudioConnection          patchCord2(VCO1_SINE, 0, VCO1_MIX, 0);
+AudioConnection          patchCord3(VCO1_SAW, 0, VCO1_MIX, 1);
+AudioConnection          patchCord4(VCO3_SINE, 0, VCO3_MIX, 0);
+AudioConnection          patchCord5(VCO2_SINE, 0, VCO2_MIX, 0);
+AudioConnection          patchCord6(VCO3_SAW, 0, VCO3_MIX, 1);
+AudioConnection          patchCord7(VCO2_SAW, 0, VCO2_MIX, 1);
+AudioConnection          patchCord8(VCO1_SQUARE, 0, VCO1_MIX, 2);
+AudioConnection          patchCord9(VCO1_TRIANGLE, 0, VCO1_MIX, 3);
+AudioConnection          patchCord10(VCO3_SQUARE, 0, VCO3_MIX, 2);
+AudioConnection          patchCord11(VCO2_SQUARE, 0, VCO2_MIX, 2);
+AudioConnection          patchCord12(VCO3_TRIANGLE, 0, VCO3_MIX, 3);
+AudioConnection          patchCord13(VCO2_TRIANGLE, 0, VCO2_MIX, 3);
+AudioConnection          patchCord14(VCO1_MIX, VCO1_AMP);
+AudioConnection          patchCord15(VCO3_MIX, VCO3_AMP);
+AudioConnection          patchCord16(VCO2_MIX, VCO2_AMP);
+AudioConnection          patchCord17(VCO1_AMP, 0, VCO_MIXER, 0);
+AudioConnection          patchCord18(NOISE_AMP, 0, VCO_MIXER, 3);
+AudioConnection          patchCord19(VCO3_AMP, 0, VCO_MIXER, 2);
+AudioConnection          patchCord20(VCO3_AMP, VCO3_AMP_2);
+AudioConnection          patchCord21(VCO2_AMP, 0, VCO_MIXER, 1);
+AudioConnection          patchCord22(VCO_MIXER, AMP_PRE_FILTER);
+AudioConnection          patchCord23(AMP_PRE_FILTER, 0, FILTER, 0);
+AudioConnection          patchCord24(AMP_PRE_FILTER, 0, MIXER_FINAL, 2);
+AudioConnection          patchCord25(VCO3_AMP_2, 0, MIXER_FILTER, 3);
+AudioConnection          patchCord26(VCO3_AMP_2, 0, MIXER_FINAL, 3);
+AudioConnection          patchCord27(FILTER, 0, MIXER_FILTER, 0);
+AudioConnection          patchCord28(FILTER, 1, MIXER_FILTER, 1);
+AudioConnection          patchCord29(FILTER, 2, MIXER_FILTER, 2);
+AudioConnection          patchCord30(MIXER_FILTER, AMP_POST_FILTER);
+AudioConnection          patchCord31(AMP_POST_FILTER, ENVELOPE);
+AudioConnection          patchCord32(AMP_POST_FILTER, 0, MIXER_FINAL, 1);
+AudioConnection          patchCord33(ENVELOPE, AMP_ENVELOPE);
+AudioConnection          patchCord34(AMP_ENVELOPE, 0, MIXER_FINAL, 0);
+AudioConnection          patchCord35(MIXER_FINAL, AMP_FINAL);
+AudioConnection          patchCord36(AMP_FINAL, 0, i2s1, 0);
+AudioConnection          patchCord37(AMP_FINAL, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=116,47
 // GUItool: end automatically generated code
 
 
+double the_volume = 0.25;
+
+double vco_1_freq = 440.0;
+double vco_delta  = 0.0;
+double vco_2_freq = 440.0 + vco_delta;
+double vco_3_freq = vco_1_freq / 2.0;
 
 
-#define ENUM_VCO1_SIN_AMP      0.25
-#define ENUM_VCO1_SAW_AMP      0.25
-#define ENUM_VCO1_SQUARE_AMP   0.25
-#define ENUM_VCO1_TRIANGLE_AMP 0.25
+double vco1_mix_sine_amp     = 0.25;
+double vco1_mix_saw_amp      = 0.25;
+double vco1_mix_square_amp   = 0.25;
+double vco1_mix_triangle_amp = 0.25;
 
-#define ENUM_VCO2_SIN_AMP      0.25
-#define ENUM_VCO2_SAW_AMP      0.25
-#define ENUM_VCO2_SQUARE_AMP   0.25
-#define ENUM_VCO2_TRIANGLE_AMP 0.25
+double vco2_mix_sine_amp     = 0.25;
+double vco2_mix_saw_amp      = 0.25;
+double vco2_mix_square_amp   = 0.25;
+double vco2_mix_triangle_amp = 0.25;
 
-#define ENUM_VCO3_SIN_AMP      0.25
-#define ENUM_VCO3_SAW_AMP      0.25
-#define ENUM_VCO3_SQUARE_AMP   0.25
-#define ENUM_VCO3_TRIANGLE_AMP 0.25
+double vco3_mix_sine_amp     = 0.25;
+double vco3_mix_saw_amp      = 0.25;
+double vco3_mix_square_amp   = 0.25;
+double vco3_mix_triangle_amp = 0.25;
+
+
 
 #define ENUM_VCO1_SIN_ID       0
 #define ENUM_VCO1_SAW_ID       1
@@ -138,9 +129,15 @@ double vco3_amp_2 = 0.0;
 #define ENUM_VCO3_ID  2
 #define ENUM_NOSIE_ID 3
 
+double vco1_mix_amp  = 0.0;
+double vco2_mix_amp  = 0.0;
+double vco3_mix_amp  = 0.0;
+double noise_mix_amp = 0.0;
+
 double amp_pre_filter =   1.0;
 double filter_freq    = 440.0;
 double filter_q       =   0.707;
+double filter_octaves =   2.5;
 
 double mixer_filter_low_pass  = 0.0;
 double mixer_filter_band_pass = 0.0;
@@ -151,6 +148,11 @@ double mixer_filter_vco3      = 0.0;
 #define ENUM_MIXER_ID_BAND_PASS 1
 #define ENUM_MIXER_ID_HIGH_PASS 2
 #define ENUM_MIXER_ID_LFO3      3
+
+double mixer_low_pass  = 0.25;
+double mixer_band_pass = 0.25;
+double mixer_high_pass = 0.25;
+double mixer_lfo3      = 0.25;
 
 double amp_post_filter = 1.0;
 
@@ -168,7 +170,7 @@ double envelope_amp             =   1.0;
 #define ENUM_MIXER_FINAL_NO_FILTER   2
 #define ENUM_MIXER_FINAL_VCO3        3
 
-double mixer_final_all         = 0.0;
+double mixer_final_all         = 1.0;
 double mixer_final_no_envelope = 0.0;
 double mixer_final_no_filter   = 0.0;
 double mixer_final_vco3        = 0.0;
@@ -178,6 +180,87 @@ double amp_final = 1.0;
 void setup() {
   // put your setup code here, to run once:
   AudioMemory(1300);
+
+  double starting_amp = 0.0;
+
+  VCO1_SINE.begin(     starting_amp, WAVEFORM_SINE,     vco_1_freq);
+  VCO2_SINE.begin(     starting_amp, WAVEFORM_SINE,     vco_2_freq);
+  VCO3_SINE.begin(     starting_amp, WAVEFORM_SINE,     vco_3_freq);
+
+  VCO1_SAW.begin(      starting_amp, WAVEFORM_SAWTOOTH, vco_1_freq);
+  VCO2_SAW.begin(      starting_amp, WAVEFORM_SAWTOOTH, vco_2_freq);
+  VCO3_SAW.begin(      starting_amp, WAVEFORM_SAWTOOTH, vco_3_freq);
+
+  VCO1_SQUARE.begin(   starting_amp, WAVEFORM_SAWTOOTH, vco_1_freq);
+  VCO2_SQUARE.begin(   starting_amp, WAVEFORM_SAWTOOTH, vco_2_freq);
+  VCO3_SQUARE.begin(   starting_amp, WAVEFORM_SAWTOOTH, vco_3_freq);
+
+  VCO1_TRIANGLE.begin( starting_amp, WAVEFORM_TRIANGLE, vco_1_freq);
+  VCO2_TRIANGLE.begin( starting_amp, WAVEFORM_TRIANGLE, vco_2_freq);
+  VCO3_TRIANGLE.begin( starting_amp, WAVEFORM_TRIANGLE, vco_3_freq);
+
+  NOISE.amplitude(     starting_amp);
+
+
+  VCO1_MIX.gain(ENUM_VCO1_SIN_ID,      vco1_mix_sine_amp);
+  VCO1_MIX.gain(ENUM_VCO1_SAW_ID,      vco1_mix_saw_amp);
+  VCO1_MIX.gain(ENUM_VCO1_SQUARE_ID,   vco1_mix_square_amp);
+  VCO1_MIX.gain(ENUM_VCO1_TRIANGLE_ID, vco1_mix_triangle_amp);
+
+  VCO2_MIX.gain(ENUM_VCO2_SIN_ID,      vco2_mix_sine_amp);
+  VCO2_MIX.gain(ENUM_VCO2_SAW_ID,      vco2_mix_saw_amp);
+  VCO2_MIX.gain(ENUM_VCO2_SQUARE_ID,   vco2_mix_square_amp);
+  VCO2_MIX.gain(ENUM_VCO2_TRIANGLE_ID, vco2_mix_triangle_amp);
+
+  VCO3_MIX.gain(ENUM_VCO3_SIN_ID,      vco3_mix_sine_amp);
+  VCO3_MIX.gain(ENUM_VCO3_SAW_ID,      vco3_mix_saw_amp);
+  VCO3_MIX.gain(ENUM_VCO3_SQUARE_ID,   vco3_mix_square_amp);
+  VCO3_MIX.gain(ENUM_VCO3_TRIANGLE_ID, vco3_mix_triangle_amp);
+
+  VCO1_AMP.gain(   vco1_amp);
+  VCO2_AMP.gain(   vco2_amp);
+  VCO3_AMP.gain(   vco3_amp);
+  NOISE_AMP.gain(  noise_amp);
+  VCO3_AMP_2.gain( vco3_amp_2);
+
+
+  VCO_MIXER.gain(ENUM_VCO1_ID,  vco1_mix_amp);
+  VCO_MIXER.gain(ENUM_VCO2_ID,  vco2_mix_amp);
+  VCO_MIXER.gain(ENUM_VCO3_ID,  vco3_mix_amp);
+  VCO_MIXER.gain(ENUM_NOSIE_ID, noise_mix_amp);
+
+
+  AMP_PRE_FILTER.gain(amp_pre_filter);
+
+  FILTER.frequency(filter_freq);
+  FILTER.resonance(filter_q);
+  FILTER.octaveControl(filter_octaves);
+
+  MIXER_FILTER.gain(ENUM_MIXER_ID_LOW_PASS,  mixer_low_pass);
+  MIXER_FILTER.gain(ENUM_MIXER_ID_BAND_PASS, mixer_band_pass);
+  MIXER_FILTER.gain(ENUM_MIXER_ID_HIGH_PASS, mixer_high_pass);
+  MIXER_FILTER.gain(ENUM_MIXER_ID_LFO3,      mixer_lfo3);
+
+  AMP_POST_FILTER.gain(amp_post_filter);
+
+  ENVELOPE.delay(         envelope_delay_ms);
+  ENVELOPE.attack(        envelope_attack_ms);
+  ENVELOPE.hold(          envelope_hold_ms);
+  ENVELOPE.decay(         envelope_decay_ms);
+  ENVELOPE.sustain(       envelope_sustain_level);
+  ENVELOPE.release(       envelope_release_ms);
+  ENVELOPE.releaseNoteOn( envelope_relaseNoteOn_ms);
+  AMP_ENVELOPE.gain(      envelope_amp);
+
+  MIXER_FINAL.gain(ENUM_MIXER_FINAL_ALL,         mixer_final_all);
+  MIXER_FINAL.gain(ENUM_MIXER_FINAL_NO_ENVELOPE, mixer_final_no_envelope);
+  MIXER_FINAL.gain(ENUM_MIXER_FINAL_NO_FILTER,   mixer_final_no_filter);
+  MIXER_FINAL.gain(ENUM_MIXER_FINAL_VCO3,        mixer_final_vco3);
+
+  AMP_FINAL.gain(amp_final);
+
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(the_volume);
 
 }
 
