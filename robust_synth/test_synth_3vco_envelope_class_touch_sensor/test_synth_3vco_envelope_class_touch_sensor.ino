406635aaa3f572055f2a851261874deba7c41a65
Daniel M. Lofaro <dan@danlofaro.com>
2023-01-19 17:36:34 -0500

    added a random voice changer via the knob, seems to work well

--- robust_synth/test_synth_3vco_envelope_class_touch_sensor/test_synth_3vco_envelope_class_touch_sensor.ino
+++ robust_synth/test_synth_3vco_envelope_class_touch_sensor/test_synth_3vco_envelope_class_touch_sensor.ino
@@ -17,26 +17,69 @@ void setup() {
 }
 
 int n = 0;
+
+
+double getRand()
+{
+  int the_rand = random();
+  double v = (double)(the_rand & 0xffff) / (double)(0xffff);
+  return v;
+}
+
+int prev_rand = 0;
+int doRand()
+{
+  double vol = sv.getVolumeKnob();
+  int the_rand = 0;
+  if ( vol > 0.5 ) the_rand = 1;
+  else             the_rand = 0;
+
+  int do_rand = 0;
+
+  if ( ( the_rand == 1 ) & ( prev_rand == 0 ) ) do_rand = 1;
+  else do_rand = 0;
+  prev_rand = the_rand;
+
+  if(do_rand == 1)
+  {
+    double v = getRand();
+    for (int vco = 0; vco < NUM_VCO; vco++)
+    {
+      for (int type = 0; type < NUM_VOICES; type++)
+      {
+        double vol = getRand();
+        if( vol < 0.5 ) vol = 0.0;
+        sv.setVolume( vco,  type,  vol);
+      }
+    }
+    SerialUSB.println("New Voice");
+  }
+  return do_rand;
+}
 void loop() {
   // put your main code here, to run repeatedly:
 
-  double vol = sv.getVolumeKnob();
-  sv.setVolume(vol*2.0);
+  //double vol = sv.getVolumeKnob();
+  //sv.setVolume(vol*2.0);
   
   
   int n = 60; 
   
-  int note = 0;
-  int t = to.getTouch(&note);
+  int note     = 0;
+  int t        = to.getTouch(&note);
   int the_note = to.getNote(note);
-  SerialUSB.println(the_note);
+  
+  //SerialUSB.println(the_note);
+  doRand();
+  
   if ( the_note > 0 )
   {
     double freq = nf.note( n + the_note );
     sv.setFreq(freq);
   }
 
-  if     ( ( t == TOUCH_START )                       ) { sv.noteOn();  SerialUSB.println("noteOn");  }
-  else if( ( t == TOUCH_STOP  ) | ( t == TOUCH_NONE ) ) { sv.noteOff(); SerialUSB.println("noteOff"); }
+  if     ( ( t == TOUCH_START ) ) { sv.noteOn();  SerialUSB.println("noteOn");  }
+  else if( ( t == TOUCH_STOP  ) ) { sv.noteOff(); SerialUSB.println("noteOff"); }
+  else if( ( t == TOUCH_NONE  ) ) { sv.noteOff();                               }
   
 }
