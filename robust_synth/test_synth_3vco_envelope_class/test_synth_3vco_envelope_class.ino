e0fd36a76aeb83d75b8e87614b17fa64515f94da
Daniel M. Lofaro <dan@danlofaro.com>
2023-01-18 22:07:03 -0500

    added note frequencies and made it play all 108 of them

--- robust_synth/test_synth_3vco_envelope_class/test_synth_3vco_envelope_class.ino
+++ robust_synth/test_synth_3vco_envelope_class/test_synth_3vco_envelope_class.ino
@@ -1,36 +1,31 @@
 
 #include "synth_vco.h"
+#include "note_freq.h"
 
 SynthVco sv = SynthVco();
+NoteFreq nf = NoteFreq();
 
 void setup() {
   SerialUSB.begin(115200);
   sv.setup();
 }
 
-double freq = 440.0;
-
+int n = 0;
 void loop() {
   // put your main code here, to run repeatedly:
 
   double vol = sv.getVolumeKnob();
   
-  freq = 440.0;
-  sv.setFreq(freq);
-  sv.setVolume(vol);
-  //SerialUSB.println(freq);
-  sv.noteOn();
-  delay(100);
-  sv.noteOff();
-  delay(200);
+  double freq = nf.note(n);
   
-  freq = 880.0;
+  n++;
+  if ( n >= NUM_NOTES ) n = 0 ;
+  
   sv.setFreq(freq);
   sv.setVolume(vol);
-  //SerialUSB.println(freq);
+  SerialUSB.println(freq);
   sv.noteOn();
   delay(100);
   sv.noteOff();
   delay(200);
-  SerialUSB.println(vol);
 }
